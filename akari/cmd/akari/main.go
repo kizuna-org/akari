5d582cc6a1c866ce34c3862f5b32d697601b0683
shiron <shiron4710.dev@gmail.com>
2025-10-19 03:02:42 +0900

    style: fmt

--- akari/cmd/akari/main.go
+++ akari/cmd/akari/main.go
@@ -23,29 +23,29 @@ func setupLogger(envMode config.EnvMode) {
 	switch envMode {
 	case config.EnvModeDevelopment:
 		opts := &slog.HandlerOptions{
-			Level:      slog.LevelDebug,
-			AddSource:  false,
+			Level:       slog.LevelDebug,
+			AddSource:   false,
 			ReplaceAttr: nil,
 		}
 		handler = slog.NewTextHandler(os.Stdout, opts)
 	case config.EnvModeProduction:
 		opts := &slog.HandlerOptions{
-			Level:      slog.LevelInfo,
-			AddSource:  false,
+			Level:       slog.LevelInfo,
+			AddSource:   false,
 			ReplaceAttr: nil,
 		}
 		handler = slog.NewJSONHandler(os.Stdout, opts)
 	case config.EnvModeTest:
 		opts := &slog.HandlerOptions{
-			Level:      slog.LevelDebug,
-			AddSource:  false,
+			Level:       slog.LevelDebug,
+			AddSource:   false,
 			ReplaceAttr: nil,
 		}
 		handler = slog.NewTextHandler(os.Stdout, opts)
 	default:
 		opts := &slog.HandlerOptions{
-			Level:      slog.LevelInfo,
-			AddSource:  false,
+			Level:       slog.LevelInfo,
+			AddSource:   false,
 			ReplaceAttr: nil,
 		}
 		handler = slog.NewTextHandler(os.Stdout, opts)
