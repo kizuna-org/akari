e43bcbd36e109a6942d72d19331c72e1ea640d18
Souma <101255979+5ouma@users.noreply.github.com>
2025-11-06 15:01:31 +0900

    feat(akari): Create database migration files with Atlas
    Generate and migarte with Makefile.

--- akari/internal/ent/generate.go
+++ akari/internal/ent/generate.go
@@ -1,3 +1,3 @@
 package ent
 
-//go:generate go run -mod=mod entgo.io/ent/cmd/ent generate --target ../../gen/ent --feature sql/versioned-migration github.com/kizuna-org/akari/internal/ent/schema
+//go:generate go run -mod=mod entgo.io/ent/cmd/ent generate --target ../../gen/ent github.com/kizuna-org/akari/internal/ent/schema
